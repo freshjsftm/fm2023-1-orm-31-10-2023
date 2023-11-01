eb77f5c30fe9cb4bd9cd7f02639cc0cf0b41733f
Olena Zdanovska <freshjs@gmail.com>
2023-11-01 10:53:11 +0200

    Add read all & create user

---
+++ controllers/user.controller.js
@@ -0,0 +1,38 @@
+const { User } = require('../models');
+
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const { body } = req;
+    const createdUser = await User.create(body);
+    createdUser.password = undefined;
+    res.status(201).send({ data: createdUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const {
+      query: { male },
+    } = req;
+    const users = await User.findAll({
+      where: {
+        isMale: male || true,
+      },
+      attributes: {
+        exclude: ['password', 'updatedAt'],
+      },
+    });
+    res.status(200).send({ data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// module.exports.createUser = async (req, res, next) => {
+//   try {
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const UserController = require('../controllers/user.controller');
 
 const router = Router();
 
 // http://localhost:3000/api/users
 //router.get('/users')
 
-module.exports = router;
+// create user
+router.post('/users', UserController.createUser);
+router.get('/users', UserController.getAllUsers);
 
+module.exports = router;
